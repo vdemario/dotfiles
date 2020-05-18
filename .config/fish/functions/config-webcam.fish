d47cd4cfe224655f6751d36f056a731d80334886
Vitor De Mario <vitor@jusbrasil.com.br>
2020-05-18 13:25:27 -0300

    Pass webcam zoom level as parameter
    To be able to change the level of zoom more easily
    without losing the rest of the webcam configs.

--- .config/fish/functions/config-webcam.fish
+++ .config/fish/functions/config-webcam.fish
@@ -1,4 +1,10 @@
 function config-webcam
+	if count $argv > /dev/null
+		set zoom $argv
+	else
+		set zoom 120
+	end
+
 	if test -e /usr/bin/v4l2-ctl
 		# find out which /dev/videoN the logitech camera is on	
 		set dev_video_id (/usr/bin/v4l2-ctl --list-devices | grep 'C920' -A 1 | grep '/dev/' | cut -d '/' -f 3)
@@ -7,6 +13,6 @@ function config-webcam
 		/usr/bin/v4l2-ctl --device=/dev/$dev_video_id --set-ctrl=focus_auto=0
 		
 		# set zoom to focus only on the face
-		/usr/bin/v4l2-ctl --device=/dev/$dev_video_id --set-ctrl=zoom_absolute=130
+		/usr/bin/v4l2-ctl --device=/dev/$dev_video_id --set-ctrl=zoom_absolute=$zoom
 	end
 end
