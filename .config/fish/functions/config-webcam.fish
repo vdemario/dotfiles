073bebb380cee95eedca3e1de5973a121cdaeb8c
Vitor De Mario <vitor@jusbrasil.com.br>
2023-08-28 14:51:33 -0300

    Improve config_webcam script

--- .config/fish/functions/config-webcam.fish
+++ .config/fish/functions/config-webcam.fish
@@ -5,14 +5,20 @@ function config-webcam
 		set zoom 120
 	end
 
-	if test -e /usr/bin/v4l2-ctl
-		# find out which /dev/videoN the logitech camera is on	
-		set dev_video_id (/usr/bin/v4l2-ctl --list-devices | grep 'C920' -A 1 | grep '/dev/' | cut -d '/' -f 3)
-		
-		# disable the auto focus
-		/usr/bin/v4l2-ctl --device=/dev/$dev_video_id --set-ctrl=focus_auto=0
-		
-		# set zoom to focus only on the face
-		/usr/bin/v4l2-ctl --device=/dev/$dev_video_id --set-ctrl=zoom_absolute=$zoom
+	# test the binary exists
+	if not test -e /usr/bin/v4l2-ctl
+		echo "v4l2-ctl not found" >&2
+		return 1
 	end
+
+	# v4l2-ctl exists, but we'll use the alias that discards stderr messages
+
+	# find out which /dev/videoN the logitech camera is on	
+	set dev_video_id (v4l2 --list-devices | grep 'C920' -A 1 | grep '/dev/' | cut -d '/' -f 3)
+	
+	# disable the auto focus
+	v4l2 --device=/dev/$dev_video_id --set-ctrl=focus_auto=0
+	
+	# set zoom to focus only on the face
+	v4l2 --device=/dev/$dev_video_id --set-ctrl=zoom_absolute=$zoom
 end
